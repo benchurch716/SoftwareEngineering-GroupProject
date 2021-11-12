9d6adcdd3299f8519eefeff729b7fc12bc5dbac3
benchurch716 <churchbe@oregonstate.edu>
2021-11-11 21:12:01 -0800

    validation started

--- task.py
+++ task.py
@@ -1,3 +1,4 @@
+import string
 # Function 1 Specification - BEN
 # This function must have the following header: def conv_num(num_str).
 # This function takes in a string and converts it into a base 10 number and returns it. It has the following specifications:
@@ -9,13 +10,102 @@
 # Negative numbers are indicated with a - like -0xFF
 # The type returned must match the type sent.
 # For example, if an string of an integer is passed in, conv_num must return an int.
+# 
 # Invalid formats should return None, including, but not limited to:
 # strings with multiple decimal points
 # strings with alpha that aren't part of a hexadecimal number
 # strings with a hexadecimal number without the proceeding 0x
 # values for num_str that are not strings or are empty strings
+
+# This function checks if the passed value is a valid integer for conv_num
+# receives: num_str - The string to be converted
+#            isPositive - true if the string doesn't start with a "-"
+# returns: True - If the input only contains numbers
+#          False - If the data is invalid 
+def _validate_int(num_str, isPositive):
+    if isPositive:
+        if num_str.isdigit():
+            return True
+    else:
+        # check all but the '-'
+        if (num_str[1:].isdigit()):
+            return True
+    return False
+
+
+def _convert_int(num_str, isPositive):
+    return "i'm an int"
+
+
+def _validate_float(num_str, isPositive):
+    # check for more than 1 decimal places
+    decimal_counter = 0
+    for char in num_str:
+        if char == ".":
+            decimal_counter += 1
+    if (decimal_counter > 1):
+        return False
+    elif(decimal_counter == 1):
+        # Check for non digits
+        if isPositive:
+            num_str = num_str.replace('.','')
+            if(num_str.isdigit):
+                return True
+            else:
+                return False
+        else:
+            num_str = num_str.replace('.','')
+            # check all but the '-'
+            if (num_str[1:].isdigit()):
+                return True
+            else:
+                return False
+    else:
+        return False
+
+def _convert_float(num_str, isPositive):
+    return "i'm a float"
+
+def _validate_hex(num_str, isPositive):
+    if "0x" in num_str:
+        # check for non hex letters after the "0x"
+        if isPositive:
+            if all(char in string.hexdigits for char in num_str[2:]):
+                return True
+            else: 
+                return False
+        else:
+            # check all but the '-0x'
+            if all(char in string.hexdigits for char in num_str[3:]):
+                return True
+            else:
+                return False
+    return False
+
+def _convert_hex(num_str, isPositive):
+    return "i'm a hex"
+
 def conv_num(num_str):
-    return
+    # check for not a string
+    if type(num_str) is not str:
+        return None
+    # check for empty string
+    if (len(num_str) == 0):
+        return None
+    # check for negative
+    isPositive = True
+    if (num_str.startswith('-')):
+        isPositive = False
+    # check int
+    if(_validate_int(num_str, isPositive)):
+        return _convert_int(num_str, isPositive)
+    # check for Float
+    if(_validate_float(num_str, isPositive)):
+        return _convert_float(num_str, isPositive)
+    # check for Hex
+    if(_validate_hex(num_str, isPositive)):
+        return _convert_hex(num_str, isPositive)
+    return None
 
 
 # Function 2 Specification - TROY
--- tests.py
+++ tests.py
@@ -1,10 +1,36 @@
 import unittest
-
+from task import conv_num
 
 class TestCase(unittest.TestCase):
+    # Tests for conv_num():
+    # reject an empty string
+    def test_empty_string(self):
+        self.assertIsNone(conv_num(""))
+    
+    # reject not a string with a regular int
+    def test_int(self):
+        self.assertIsNone(conv_num(123))
+    
+    def test_string_int(self):
+        self.assertEqual(conv_num("123"), "i'm an int")
 
-    def test1(self):
-        self.assertTrue(True)
+    def test_negative_int(self):
+        self.assertEqual(conv_num("-123"), "i'm an int")
+    
+    def test_float(self):
+        self.assertEqual(conv_num("12.345"), "i'm a float")
+    
+    def test_negative_float(self):
+        self.assertEqual(conv_num("-12.345"), "i'm a float")
+    
+    def test_multiple_decimal_points(self):
+        self.assertIsNone(conv_num("12.1.23"))
+    
+    def test_hex(self):
+        self.assertEqual(conv_num("0x1a2"), "i'm a hex")
+
+    def test_negative_hex(self):
+        self.assertEqual(conv_num("-0x1a2"), "i'm a hex")
 
 
 if __name__ == '__main__':
