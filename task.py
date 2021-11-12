35bd1914a74041453afac54f419a5a2cbce39587
benchurch716 <churchbe@oregonstate.edu>
2021-11-11 17:16:28 -0800

    fixed line 24

--- task.py
+++ task.py
@@ -21,7 +21,7 @@ def conv_num(num_str):
 # Function 2 Specification - TROY
 # This function must have the following header: def my_datetime(num_sec).
 # This function takes in an integer value that represents the number of seconds since the epoch: January 1st 1970.
-# The function takes num_sec and converts it to a date and returns it as a string with the following format: MM-DD-YYYY. 
+# The function takes num_sec and converts it to a date and returns it as a string with the following format: MM-DD-YYYY.
 
 # It has the following specifications:
 # It may be assumed that num_sec will always be an int value
