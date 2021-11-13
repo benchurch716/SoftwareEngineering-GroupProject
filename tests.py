cda98aad34af9f51148f9d115eeb1c2a16e722f4
benchurch716 <benchurch716@gmail.com>
2021-11-12 19:56:43 -0800

    working

--- tests.py
+++ tests.py
@@ -1,31 +1,33 @@
 import unittest
 from task import conv_num
 
+
 class TestCase(unittest.TestCase):
     # Tests for conv_num():
     # reject an empty string
     def test_empty_string(self):
         self.assertIsNone(conv_num(""))
-    
-    # reject not a string with a regular int
+
+    # reject a regular int
     def test_int(self):
         self.assertIsNone(conv_num(123))
-    
+
     def test_string_int(self):
         self.assertEqual(conv_num("123"), "i'm an int")
 
     def test_negative_int(self):
         self.assertEqual(conv_num("-123"), "i'm an int")
-    
+
     def test_float(self):
         self.assertEqual(conv_num("12.345"), "i'm a float")
-    
+
     def test_negative_float(self):
         self.assertEqual(conv_num("-12.345"), "i'm a float")
-    
+
+    # reject more than 1 decimal points
     def test_multiple_decimal_points(self):
         self.assertIsNone(conv_num("12.1.23"))
-    
+
     def test_hex(self):
         self.assertEqual(conv_num("0x1a2"), "i'm a hex")
 
