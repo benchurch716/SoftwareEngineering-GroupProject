21070880d07698e6b7fdd1a7c01972cbb5e4d7c8
Troy Peele <TroyPeele2@users.noreply.github.com>
2021-11-17 12:34:52 -0500

    function2

--- task.py
+++ task.py
@@ -160,41 +160,31 @@ def my_datetime(num_sec):
     months = {
         1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
         7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-
     current_month, current_day, current_year = 1, 1, 1970
     seconds_in_day = 86400
     total_days = math.ceil(num_sec/seconds_in_day)
     leaps = 0
-
     while total_days > 0:
-
         if current_month == 2 and current_year >= 1972 and leap_check(current_year):
             months[2] = 29
             leaps += 1
-
         if total_days > months[current_month]:
             total_days -= months[current_month]
             current_month += 1
-
             if current_month > 12:
                 current_month = 1
                 current_year += 1
                 months[2] = 28
-
         elif total_days + current_day == months[current_month] and current_month == 2:
             current_day = int(total_days) + 1
             total_days = 0
-
         elif total_days < months[current_month]:
             current_day = int(total_days)
             total_days = 0
-
     if current_month < 10:
         current_month = '0' + str(current_month)
-
     if current_day < 10:
         current_day = '0' + str(current_day)
-
     end_date = str(current_month) + '-' + str(current_day) + '-' + str(current_year)
     return end_date
 
@@ -205,8 +195,6 @@ def leap_check(year):
         return False
     elif (year - 1972) % 4 == 0:
         return True
-
-
 # Function 3 Specification - PAUL
 # This function must have the following header:
 # def conv_endian(num, endian='big').
